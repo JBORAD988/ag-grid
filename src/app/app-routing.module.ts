46d069e5814cd1cf86327407f869ca8d74922b91
jayborad <co-20-081@rctiahmedabad.gujgov.edu.in>
2024-02-27 12:36:29 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AgGridUseComponent} from "./ag-grid-use/ag-grid-use.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'grid', component: AgGridUseComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AgGridUseComponent } from './ag-grid-use/ag-grid-use.component';
+import {AgGridAngular} from "ag-grid-angular";
+import {HttpClientModule} from "@angular/common/http";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AgGridUseComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        AgGridAngular,
+       HttpClientModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
